1efb0560b5e497eff264302be4c1cff7e0dfb84a
pimparampimpim <114080270+pimparampimpim@users.noreply.github.com>
2023-05-16 11:19:45 +0300

    Update zadacha.sh

--- zadacha.sh
+++ zadacha.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+if [[ ! -z $1 ]]
 
-password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+then
+password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n $1)
 echo "Генерация нового пароля"
 sleep 3
-echo "Завершение генерации пароля"
+echo "Завершение генерации пароля"    
 sleep 2
 echo "Новый пароль: $password"
+fi
+exit 0
+
+else
+echo 'не хватает аргументов'
+fi
+exit 1
