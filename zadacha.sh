37f6ddb2846b082eda3a1243e2bf64992ef1fcf8
pimparampimpim <nika1273@inbox.ru>
2023-05-23 11:58:51 +0300

    tessstt

--- zadacha.sh
+++ zadacha.sh
@@ -4,9 +4,9 @@ if [[ ! -z $1 ]] && [[ ! -w $2 ]] && [[ -n $3 ]]
 then 
     password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $2 | head -n $1)
     echo "Генерация нового пароля"
-    sleep 3
+   # sleep 3
     echo "Завершение генерации пароля"    
-    sleep 2
+   # sleep 2
     echo "$password" > $3
     exit 0
 
